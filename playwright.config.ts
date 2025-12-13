1e058a458045684352e1e11ca39e1639a303bb6d
karthik9629846380@gmail.com <karthik9629846380@gmail.com>
2025-12-14 00:24:21 +0530

    parallel run to true

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,9 +25,10 @@ switch (process.env.ENV) {
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -38,6 +39,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
